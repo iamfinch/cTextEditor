c5c14cdaa7a1b99952c6f9ae6fa75c0ebfb1b6ce
iamfinch <iamfinch47@gmail.com>
2026-09-23 22:19:55 -0500

    added ability to read current attribs, modify the struct, and pass the struct to write new attribs

--- kilo.c
+++ kilo.c
@@ -1,3 +1,26 @@
-int main() {
-    return 1;
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode(void) {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode(void){
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_cflag &= ~(ECHO);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+}
+
+int main(void) {
+    enableRawMode();
+    char c;
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    return 0;
 }
