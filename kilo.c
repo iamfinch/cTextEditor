d94e02933aec86fa462497580b305f29974eee1e
iamfinch <iamfinch47@gmail.com>
2026-09-25 20:14:59 -0500

    repositioned the cursor

--- kilo.c
+++ kilo.c
@@ -63,6 +63,7 @@ void editorProcessKeypress(void) {
 /** output **/
 void editorRefreshScreen(void) {
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /** init **/
