9bd73ecf158faf8b0f1e26767f649e074e77ffe1
iamfinch <iamfinch47@gmail.com>
2026-09-25 20:10:30 -0500

    fixed misnaming and refactored input from keyboard

--- kilo.c
+++ kilo.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 
 /*** defines ***/
-#define CRTL_KEY(k) ((k) & 0x1f)
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 /** data **/
 struct termios orig_termios;
@@ -39,22 +39,34 @@ void enableRawMode(void){
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-/** init **/
+char editorReadKey(void) {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
 
+    return c;
+}
+
+/** input **/
+void editorProcessKeypress(void) {
+    char c = editorReadKey();
+
+    switch(c) {
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
+}
+
+
+/** init **/
 int main(void) {
     enableRawMode();
 
     while(1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1) die("read");
-
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf ("%d ('%c')\r\n", c, c);
-        }
-
-        if (c == CRTL_KEY('q')) break;
+        editorProcessKeypress();
     }
     return 0;
 }
